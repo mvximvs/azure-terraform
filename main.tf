e036eb8c5e54e4f9bdae1e44c357a50d0f3393e3
Maximiliano Becerra <maxbecerra@gmail.com>
2021-11-29 17:00:22 +0100

    add terraform configuration

---
+++ main.tf
@@ -0,0 +1,46 @@
+﻿terraform{
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+  name = "tfmainrg"
+  location = "francecentral"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                = "weatherapi"
+  location            = azurerm_resource_group.tf_test.location
+  resource_group_name = azurerm_resource_group.tf_test.name
+  
+  os_type             = "Linux"
+  ip_address_type     = "public"
+  dns_name_label      = "binarythistlewa"
+  
+  container {
+    name   = "weatherapi"
+    image  = "mvximvs/weatherapi"
+    cpu    = 1
+    memory = 1
+    
+    ports{
+      port = 80 
+      protocol = "TCP"
+    }
+  }
+}
+
+## Commands
+# init
+# plan
+# apply
+# destroy
